76dd06f9001931e3e938af410a2a10d6fe57ac92
ENGLISH <english@ENGLISHs-iMac.local>
2016-10-30 09:44:08 -0700

    Initial Commit

---
+++ CurrencyConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyConverter
+//
+//  Created by ENGLISH on 10/30/16.
+//  Copyright © 2016 ArtemBerezovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrencyConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyConverter
+//
+//  Created by ENGLISH on 10/30/16.
+//  Copyright © 2016 ArtemBerezovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
